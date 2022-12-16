b2cc83bb41d9b624e4ae0f2fd69d389dd0c5ef48
yoojh9 <yoojh9@gmail.com>
2022-12-16 16:32:14 +0900

    과제

--- react-component-guide/src/components/ExpenseItem.js
+++ react-component-guide/src/components/ExpenseItem.js
@@ -1,17 +1,10 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-    const month = props.date.toLocaleString("ko-KR", { month: "long" });
-    const day = props.date.toLocaleString("ko-KR", { day: "2-digit" });
-    const year = props.date.getFullYear();
-
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
