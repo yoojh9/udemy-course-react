4e151fc93ce5cbb205a08b2cc9ed286bd25b02d0
yoojh9 <yoojh9@gmail.com>
2022-12-20 15:13:51 +0900

    객체 state

---
+++ react-component-guide/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,10 @@
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css'
+
+const NewExpense = () => {
+  return <div className='new-expense'>
+    <ExpenseForm/>
+  </div>
+}
+
+export default NewExpense;
