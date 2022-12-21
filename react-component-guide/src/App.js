de2b2ab1eb83b5d1452d6a0e1836770a58e94448
yoojh9 <yoojh9@gmail.com>
2022-12-21 10:02:18 +0900

    자식 대 부모 컴포넌트 통신(상향식

--- react-component-guide/src/App.js
+++ react-component-guide/src/App.js
@@ -30,9 +30,13 @@ const App = () => {
         },
     ];
 
+    const addExpenseHandler = expense => {
+        console.log(expense);
+    }
+
     return (
         <div>
-            <NewExpense/>
+            <NewExpense onAddExpense={addExpenseHandler}/>
             <Expenses items={expenses} />
         </div>
     );
