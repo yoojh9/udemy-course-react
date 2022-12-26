c6b137cc41b242f71346bb1efa9840a89fcb090b
yoojh9 <yoojh9@gmail.com>
2022-12-26 15:03:15 +0900

    Chart 추가하기

--- react-component-guide/src/components/Expenses/Expenses.js
+++ react-component-guide/src/components/Expenses/Expenses.js
@@ -2,12 +2,11 @@ import { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
-
+import ExpensesChart from "./ExpensesChart";
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState("2022");
-    
+
     const filterChangeHandler = (selectedYear) => {
-        console.log(selectedYear);
         setFilteredYear(selectedYear);
     };
 
@@ -22,6 +21,7 @@ const Expenses = (props) => {
                 selected={filteredYear}
                 onChangeFilter={filterChangeHandler}
             />
+            <ExpensesChart expenses={filteredExpenses}/>
             <ExpensesList items={filteredExpenses}/>
         </Card>
     );
--- react-component-guide/src/components/NewExpense/ExpenseForm.js
+++ react-component-guide/src/components/NewExpense/ExpenseForm.js
@@ -40,7 +40,7 @@ const ExpenseForm = (props) => {
     event.preventDefault();
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate)
     }
 
