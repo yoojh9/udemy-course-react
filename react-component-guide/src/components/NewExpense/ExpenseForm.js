07a2152ce925844d57529465e02310c849712aa5
yoojh9 <yoojh9@gmail.com>
2022-12-26 10:48:52 +0900

    Conditional Conent

--- react-component-guide/src/components/NewExpense/ExpenseForm.js
+++ react-component-guide/src/components/NewExpense/ExpenseForm.js
@@ -68,6 +68,7 @@ const ExpenseForm = (props) => {
       </div>
     </div>
     <div className='new-expense__actions'>
+      <button type="button" onClick={props.onClickBtn}>Cancel</button>
       <button type="submit">Add Expense</button>
     </div>
   </form>
--- react-component-guide/src/components/NewExpense/NewExpense.js
+++ react-component-guide/src/components/NewExpense/NewExpense.js
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css'
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
 
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
@@ -9,10 +11,20 @@ const NewExpense = (props) => {
       id: Math.random().toString()
     }
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  }
+
+  const changeIsEditing = () => {
+    setIsEditing(prev => !prev);
   }
 
   return <div className='new-expense'>
-    <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+    {
+      isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onClickBtn={changeIsEditing}/>
+    }
+    {
+      isEditing && <button onClick={changeIsEditing}>New Add Expense</button>
+    }
   </div>
 }
 
