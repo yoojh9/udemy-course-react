08df7793cc294363b5fed37c086dac7d3dc48ce3
yoojh9 <yoojh9@gmail.com>
2022-12-26 15:51:07 +0900

    인라인 스타일

--- react-component-styling/README.md
+++ react-component-styling/README.md
@@ -4,3 +4,13 @@
 - Styled Components
 - CSS Modules
 
+<br><br>
+
+## 2. 동적 인라인 스타일 설정하기
+
+- 현재 CSS 방식은 CourseGoalList.css를 CourseGoalList.js에서만 import 했다 하더라도 .goal-list가 있는 전체 페이지의 모든 요소에 영향을 미친다.
+- 인라인 방식이 100% 만족스럽지 않은 이유는 인라인 스타일 방식은 최우선 순위를 차지하기 때문이다. 개인적으로 inline 스타일은 선호하지 않는다.
+
+```javascript
+<label style={{ color: !isValid ? "red" : "#ccc" }}>Course Goal</label>
+```
--- react-component-styling/src/components/CourseGoals/CourseInput/CourseInput.js
+++ react-component-styling/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
+    if(event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: !isValid ? "red" : "#ccc" }}>Course Goal</label>
+        <input
+          type="text"
+          onChange={goalInputChangeHandler}
+          style={{
+            borderColor: !isValid && "red",
+            background: !isValid ? "salmon" : "transparent",
+          }}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
