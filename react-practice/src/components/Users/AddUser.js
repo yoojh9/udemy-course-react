e9c77504db1f4446a562cb592f3bab07a8ce8487
yoojh9 <yoojh9@gmail.com>
2023-01-05 09:25:02 +0900

    재사용 가능한 Button 컴포넌트 추가하기

--- react-practice/src/components/Users/AddUser.js
+++ react-practice/src/components/Users/AddUser.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from '../UI/Card';
+import Button from '../UI/Button';
 import styles from './AddUser.module.css'
 
 const AddUser = props => {
@@ -15,7 +16,7 @@ const AddUser = props => {
         <input id="username" type="text"></input>
         <label htmlFor='age'>Age (Years)</label>
         <input id="age" type="number"></input>
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   )
