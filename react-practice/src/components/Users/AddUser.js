cb3342d378952b5682245d1a1386acc879ebabb4
yoojh9 <yoojh9@gmail.com>
2023-01-09 11:10:30 +0900

    Error State 관리하기

--- react-practice/src/components/Users/AddUser.js
+++ react-practice/src/components/Users/AddUser.js
@@ -7,6 +7,7 @@ import ErrorModal from "../UI/ErrorModal";
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState("");
     const [enteredAge, setEnteredAge] = useState("");
+    const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
@@ -14,9 +15,17 @@ const AddUser = (props) => {
             enteredUsername.trim().length === 0 ||
             enteredAge.trim().length === 0
         ) {
+            setError({
+                title: "Invalid Input",
+                message: "Please enter a valid name and age (non-empty value).",
+            });
             return;
         }
         if (+enteredAge < 1) {
+            setError({
+                title: "Invalid age",
+                message: "Please enter a valid age (> 0.)",
+            });
             return;
         }
         props.onAddUser(enteredUsername, enteredAge);
@@ -32,12 +41,19 @@ const AddUser = (props) => {
         setEnteredAge(event.target.value);
     };
 
+    const errorHandler = () => {
+        setError(null);
+    };
+
     return (
         <div>
-            <ErrorModal
-                title="An error occured!"
-                message="Something went wrong!"
-            />
+            {error && (
+                <ErrorModal
+                    title={error.title}
+                    message={error.message}
+                    onConfirm={errorHandler}
+                />
+            )}
             {/* Card라는 사용자가 직접 만든 component에 className을 사용하면 jsx의 className 프로퍼티를 사용하는게 아니라 props로 인식이 된다. */}
             <Card className={styles.input}>
                 <form onSubmit={addUserHandler}>
