900df8c29fb2dbf3c73b8917334e580227512e56
yoojh9 <yoojh9@gmail.com>
2023-01-09 11:01:55 +0900

    Error 모달 컴포넌트 추가

---
+++ react-practice/src/components/UI/ErrorModal.js
@@ -0,0 +1,24 @@
+import Card from "./Card";
+import Button from "./Button";
+import styles from "./ErrorModal.module.css";
+
+const ErrorModal = (props) => {
+    return (
+        <div>
+            <div className={styles.backdrop} />
+            <Card className={styles.modal}>
+                <header className={styles.header}>
+                    <h2>{props.title}</h2>
+                </header>
+                <div className={styles.content}>
+                    <p>{props.message}</p>
+                </div>
+                <footer className={styles.actions}>
+                    <Button>Okay</Button>
+                </footer>
+            </Card>
+        </div>
+    );
+};
+
+export default ErrorModal;
--- react-practice/src/components/Users/AddUser.js
+++ react-practice/src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import styles from "./AddUser.module.css";
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState("");
@@ -32,26 +33,32 @@ const AddUser = (props) => {
     };
 
     return (
-        // Card라는 사용자가 직접 만든 component에 className을 사용하면 jsx의 className 프로퍼티를 사용하는게 아니라 props로 인식이 된다.
-        <Card className={styles.input}>
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input
-                    id="username"
-                    type="text"
-                    value={enteredUsername}
-                    onChange={usernameChangeHandler}
-                ></input>
-                <label htmlFor="age">Age (Years)</label>
-                <input
-                    id="age"
-                    type="number"
-                    value={enteredAge}
-                    onChange={ageChangeHandler}
-                ></input>
-                <Button type="submit">Add User</Button>
-            </form>
-        </Card>
+        <div>
+            <ErrorModal
+                title="An error occured!"
+                message="Something went wrong!"
+            />
+            {/* Card라는 사용자가 직접 만든 component에 className을 사용하면 jsx의 className 프로퍼티를 사용하는게 아니라 props로 인식이 된다. */}
+            <Card className={styles.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        id="username"
+                        type="text"
+                        value={enteredUsername}
+                        onChange={usernameChangeHandler}
+                    ></input>
+                    <label htmlFor="age">Age (Years)</label>
+                    <input
+                        id="age"
+                        type="number"
+                        value={enteredAge}
+                        onChange={ageChangeHandler}
+                    ></input>
+                    <Button type="submit">Add User</Button>
+                </form>
+            </Card>
+        </div>
     );
 };
 
