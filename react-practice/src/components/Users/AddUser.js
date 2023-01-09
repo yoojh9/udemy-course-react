03c3ce0f9aa49ddf1ead59c8dee1fcf7e54de450
yoojh9 <yoojh9@gmail.com>
2023-01-09 10:18:02 +0900

    사용자 입력 state 관리하기

--- react-practice/src/components/Users/AddUser.js
+++ react-practice/src/components/Users/AddUser.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import Card from '../UI/Card';
-import Button from '../UI/Button';
-import styles from './AddUser.module.css'
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import styles from "./AddUser.module.css";
 
-const AddUser = props => {
-  const addUserHandler = (event) => {
-    event.preventDefault();
-  }
+const AddUser = (props) => {
+    const [enteredUsername, setEnteredUsername] = useState("");
+    const [enteredAge, setEnteredAge] = useState("");
 
-  return (
-    // Card라는 사용자가 직접 만든 component에 className을 사용하면 jsx의 className 프로퍼티를 사용하는게 아니라 props로 인식이 된다.
-    <Card className={styles.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor='username'>Username</label>
-        <input id="username" type="text"></input>
-        <label htmlFor='age'>Age (Years)</label>
-        <input id="age" type="number"></input>
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
-  )
+    const addUserHandler = (event) => {
+        event.preventDefault();
+        console.log(enteredUsername, enteredAge);
+    };
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
+    };
+
+    return (
+        // Card라는 사용자가 직접 만든 component에 className을 사용하면 jsx의 className 프로퍼티를 사용하는게 아니라 props로 인식이 된다.
+        <Card className={styles.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input
+                    id="username"
+                    type="text"
+                    value={enteredUsername}
+                    onChange={usernameChangeHandler}
+                ></input>
+                <label htmlFor="age">Age (Years)</label>
+                <input
+                    id="age"
+                    type="number"
+                    value={enteredAge}
+                    onChange={ageChangeHandler}
+                ></input>
+                <Button type="submit">Add User</Button>
+            </form>
+        </Card>
+    );
 };
 
-export default AddUser
+export default AddUser;
