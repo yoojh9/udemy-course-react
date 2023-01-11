48c7cb2f0fce3110fa64f5bd81a9e4e20689864b
yoojh9 <yoojh9@gmail.com>
2023-01-11 13:46:38 +0900

    컨텍스트를 동적으로 만들기

--- react-advanced-features/src/App.js
+++ react-advanced-features/src/App.js
@@ -6,42 +6,42 @@ import MainHeader from "./components/MainHeader/MainHeader";
 import AuthContext from "./store/auth-context";
 
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    const loginHandler = (email, password) => {
-        // We should of course check email and password
-        // But it's just a dummy/ demo anyways
-        localStorage.setItem("isLoggedIn", "1");
-        setIsLoggedIn(true);
-    };
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
 
-    const logoutHandler = () => {
-        localStorage.removeItem("isLoggedIn");
-        setIsLoggedIn(false);
-    };
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
 
-    // 앱이 실행되고 한번만 실행 됨
-    useEffect(() => {
-        const storeUserLoggedInInformation = localStorage.getItem("isLoggedIn");
-        if (storeUserLoggedInInformation === "1") {
-            setIsLoggedIn(true);
-        }
-    }, []);
+  // 앱이 실행되고 한번만 실행 됨
+  useEffect(() => {
+    const storeUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (storeUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
-    return (
-        <React.Fragment>
-            {/* AuthContext.Provider는 컴포넌트로 AuthContext 컨텍스트가 필요한 컴포넌트들을 감쌀 수 있다. 
+  return (
+    <React.Fragment>
+      {/* AuthContext.Provider는 컴포넌트로 AuthContext 컨텍스트가 필요한 컴포넌트들을 감쌀 수 있다. 
                 모든 컴포넌트들은 AuthContext를 Wrapper로 사용하므로 AuthContext 컨텍스트에 접근할 수 있다       
             */}
-            <AuthContext.Provider value={{isLoggedIn}}>
-                <MainHeader onLogout={logoutHandler} />
-                <main>
-                    {!isLoggedIn && <Login onLogin={loginHandler} />}
-                    {isLoggedIn && <Home onLogout={logoutHandler} />}
-                </main>
-            </AuthContext.Provider>
-        </React.Fragment>
-    );
+      <AuthContext.Provider value={{ isLoggedIn, onLogout: logoutHandler }}>
+        <MainHeader onLogout={logoutHandler} />
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+      </AuthContext.Provider>
+    </React.Fragment>
+  );
 }
 
 export default App;
--- react-advanced-features/src/components/MainHeader/Navigation.js
+++ react-advanced-features/src/components/MainHeader/Navigation.js
@@ -3,7 +3,7 @@ import AuthContext from '../../store/auth-context';
 
 import classes from './Navigation.module.css';
 
-const Navigation = (props) => {
+const Navigation = () => {
   const ctx = useContext(AuthContext);
 
   return (
@@ -21,7 +21,7 @@ const Navigation = (props) => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
