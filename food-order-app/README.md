3846c30b6e4b51eb6013d8562d993ed2c7790b2b
yoojh9 <yoojh9@gmail.com>
2023-01-16 16:49:17 +0900

    개별 Meal 항목 추가하기

--- food-order-app/README.md
+++ food-order-app/README.md
@@ -7,3 +7,7 @@
 ### 2. 장바구니 버튼 추가하기
 
 -   https://github.com/yoojh9/udemy-course-react/commit/28d912be0d4312352fb5590fb4efcd6090203cb9
+
+### 3. 음식 컴포넌트 추가하기
+
+-   https://github.com/yoojh9/udemy-course-react/commit/061599fe7671c9cf4f6b7a40679771ed271f45c8
--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
+import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
     {
@@ -28,11 +30,20 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+    const mealsList = DUMMY_MEALS.map((meal) => (
+        <MealItem
+            key={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        />
+    ));
 
     return (
         <section className={classes.meals}>
-            <ul>{mealsList}</ul>
+            <Card>
+                <ul>{mealsList}</ul>
+            </Card>
         </section>
     );
 };
