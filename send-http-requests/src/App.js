d7049713d4454fb88b6717d74cdfd6e3237a5bf2
yoojh9 <yoojh9@gmail.com>
2023-01-25 09:31:46 +0900

    로딩 및 데이터 state 처리하기

--- send-http-requests/src/App.js
+++ send-http-requests/src/App.js
@@ -5,22 +5,24 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMovieHandler = () => {
-    fetch('https://swapi.dev/api/films').then(response => {
-      return response.json()
-    }).then(data => {
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-      })
-      setMovies(transformedMovies);
-    }).catch();
+  const fetchMovieHandler = async () => {
+    setIsLoading(true);
+    const response = await fetch('https://swapi.dev/api/films')
+    const data = await response.json()
+    const transformedMovies = data.results.map(movieData => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      }
+    })
+    setMovies(transformedMovies);
+    setIsLoading(false);
   }
+
 
   return (
     <React.Fragment>
@@ -28,7 +30,9 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        { !isLoading && movies.length > 0 && <MoviesList movies={movies} /> }
+        { !isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        { isLoading && <p>Loading...</p> }
       </section>
     </React.Fragment>
   );
