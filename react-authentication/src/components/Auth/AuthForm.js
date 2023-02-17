faeb1c8bf4f6ba8357a3b32ea5d14ef4c66cd1fb
yoojh9 <yoojh9@gmail.com>
2023-02-17 10:45:55 +0900

    사용자 가입 추가하기

--- react-authentication/src/components/Auth/AuthForm.js
+++ react-authentication/src/components/Auth/AuthForm.js
@@ -1,39 +1,90 @@
-import { useState } from 'react';
+import { useRef, useState } from "react";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
-  const [isLogin, setIsLogin] = useState(true);
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
+    const [isLogin, setIsLogin] = useState(true);
 
-  const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
-  };
+    const switchAuthModeHandler = () => {
+        setIsLogin((prevState) => !prevState);
+    };
 
-  return (
-    <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
-        <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
-        </div>
-        <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
-        </div>
-      </form>
-    </section>
-  );
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const enteredEmail = emailInputRef.current.value;
+        const enteredPassword = passwordInputRef.current.value;
+
+        // optional: Add validation
+
+        if (isLogin) {
+        } else {
+            fetch(
+                `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_API_KEY}`,
+                {
+                    method: "POST",
+                    body: JSON.stringify({
+                        email: enteredEmail,
+                        password: enteredPassword,
+                        returnSecureToken: true,
+                    }),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            )
+                .then((res) => {
+                    if (res.ok) {
+                        //...
+                    } else {
+                        res.json().then((data) => {
+                            // sown an error modal
+                            console.log(data);
+                        });
+                    }
+                })
+                .catch();
+        }
+    };
+
+    return (
+        <section className={classes.auth}>
+            <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+            <form onSubmit={submitHandler}>
+                <div className={classes.control}>
+                    <label htmlFor="email">Your Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        required
+                        ref={emailInputRef}
+                    />
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor="password">Your Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        required
+                        ref={passwordInputRef}
+                    />
+                </div>
+                <div className={classes.actions}>
+                    <button>{isLogin ? "Login" : "Create Account"}</button>
+                    <button
+                        type="button"
+                        className={classes.toggle}
+                        onClick={switchAuthModeHandler}
+                    >
+                        {isLogin
+                            ? "Create new account"
+                            : "Login with existing account"}
+                    </button>
+                </div>
+            </form>
+        </section>
+    );
 };
 
 export default AuthForm;
