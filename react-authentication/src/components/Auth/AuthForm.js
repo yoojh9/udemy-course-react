62f19b99b60599bef100c2136946abed3a750bd8
yoojh9 <yoojh9@gmail.com>
2023-02-17 15:45:51 +0900

    사용자 로그인 추가하기

--- react-authentication/src/components/Auth/AuthForm.js
+++ react-authentication/src/components/Auth/AuthForm.js
@@ -19,39 +19,44 @@ const AuthForm = () => {
 
         // optional: Add validation
         setIsLoading(true);
+        let url;
         if (isLogin) {
+            url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_FIREBASE_API_KEY}`;
         } else {
-            fetch(
-                `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_API_KEY}`,
-                {
-                    method: "POST",
-                    body: JSON.stringify({
-                        email: enteredEmail,
-                        password: enteredPassword,
-                        returnSecureToken: true,
-                    }),
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            )
-                .then((res) => {
-                    setIsLoading(false);
-                    if (res.ok) {
-                        //...
-                    } else {
-                        res.json().then((data) => {
-                            // sown an error modal
-                            let errorMessage = "Authentication failed";
-                            if (data && data.error && data.error.message) {
-                                errorMessage = data.error.message;
-                            }
-                            alert(errorMessage);
-                        });
-                    }
-                })
-                .catch();
+            url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_API_KEY}`;
         }
+        fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+                email: enteredEmail,
+                password: enteredPassword,
+                returnSecureToken: true,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((res) => {
+                setIsLoading(false);
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    return res.json().then((data) => {
+                        // sown an error modal
+                        let errorMessage = "Authentication failed";
+                        if (data && data.error && data.error.message) {
+                            errorMessage = data.error.message;
+                        }
+                        throw new Error(errorMessage);
+                    });
+                }
+            })
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((err) => {
+                alert(err.message);
+            });
     };
 
     return (
