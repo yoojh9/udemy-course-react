be5943766010f4bde97be0afd6e250ebd24bb03b
yoojh9 <yoojh9@gmail.com>
2023-02-21 14:43:56 +0900

    사용자 정의 컴포넌트 및 CSS 모듈 추가하기

---
+++ deep-dive-nextjs/pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return(
+    <MeetupDetail 
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1600px-Stadtbild_M%C3%BCnchen.jpg?20130611211153"
+      title="A First Meetup"
+      address="Some Street 5, Some city"
+      description="This is a first meetup"/>
+  )
+}
+
+export default MeetupDetails;
