166bc2967fdb7acef5a29f624ab0addfafcbdd89
yoojh9 <yoojh9@gmail.com>
2023-02-21 13:50:44 +0900

    새 Meetup 양식 추가하기

--- deep-dive-nextjs/README.md
+++ deep-dive-nextjs/README.md
@@ -1,3 +1,9 @@
 # Deep Dive NextJS
 
 ## 1) Meetup 목록 출력하기
+-   https://github.com/yoojh9/udemy-course-react/commit/54f882c90cfedbf85bd452d43585515137de5207
+
+<br><br>
+
+
+## 2) 새 Meetup 양식 추가하기
--- deep-dive-nextjs/pages/new-meetup/index.js
+++ deep-dive-nextjs/pages/new-meetup/index.js
@@ -1,1 +1,12 @@
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
 // our-domain.com/new-meetup
+const NewMeetupPage = () => {
+  const addMeetupHandler = (enteredMeetupData) => {
+    console.log(enteredMeetupData);
+  }
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+}
+
+export default NewMeetupPage;
