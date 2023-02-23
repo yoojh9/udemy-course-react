dd7637476db52463e28a471fe9d3a441b25c602f
yoojh9 <yoojh9@gmail.com>
2023-02-23 11:10:05 +0900

    연습하기

--- react-typescript/react-ts/src/components/Todos.tsx
+++ react-typescript/react-ts/src/components/Todos.tsx
@@ -1,10 +1,11 @@
 import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
     return (
         <ul>
             {props.items.map((item) => (
-                <li key={item.id}>{item.text}</li>
+                <TodoItem key={item.id} text={item.text} />
             ))}
         </ul>
     );
