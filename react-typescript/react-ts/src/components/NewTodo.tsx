563189a297ce083c85dc93457b7747ee7154db27
yoojh9 <yoojh9@gmail.com>
2023-02-23 14:42:47 +0900

    style 추가

--- react-typescript/react-ts/src/components/NewTodo.tsx
+++ react-typescript/react-ts/src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import classes from "./NewTodo.module.css";
 
 const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
     const todoTextInputRef = useRef<HTMLInputElement>(null);
@@ -15,7 +16,7 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
     };
 
     return (
-        <form onSubmit={submitHandler}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <label htmlFor="text">Todo Text</label>
             <input type="text" id="text" ref={todoTextInputRef} />
             <button>Add Todo</button>
--- react-typescript/react-ts/src/components/Todos.tsx
+++ react-typescript/react-ts/src/components/Todos.tsx
@@ -1,9 +1,10 @@
 import Todo from "../models/todo";
 import TodoItem from "./TodoItem";
+import classes from "./Todos.module.css";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
     return (
-        <ul>
+        <ul className={classes.todos}>
             {props.items.map((item) => (
                 <TodoItem key={item.id} text={item.text} />
             ))}
