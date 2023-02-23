b3daebaa5cdaddb8b4013404ea227ab17dcdb013
yoojh9 <yoojh9@gmail.com>
2023-02-23 14:36:48 +0900

    state 및 타입스크립트 관리하기

--- react-typescript/react-ts/src/App.tsx
+++ react-typescript/react-ts/src/App.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import "./App.css";
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-    const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    const addTodoHandler = (todoText: string) => {
+        const newToDo = new Todo(todoText);
+        setTodos((prev) => prev.concat(newToDo));
+    };
 
     return (
         <div className="App">
+            <NewTodo onAddTodo={addTodoHandler} />
             <Todos items={todos} />
         </div>
     );
--- react-typescript/react-ts/src/components/NewTodo.tsx
+++ react-typescript/react-ts/src/components/NewTodo.tsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 
-const NewTodo: React.FC = () => {
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
     const todoTextInputRef = useRef<HTMLInputElement>(null);
 
     const submitHandler = (event: React.FormEvent) => {
@@ -11,6 +11,7 @@ const NewTodo: React.FC = () => {
             // throw an error
             return;
         }
+        props.onAddTodo(enteredText);
     };
 
     return (
